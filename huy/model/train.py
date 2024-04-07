2d957b97a7a85f71ab85940b03f2e10199d4b6c5
Huy Thai Gia <54904187+HweyTH@users.noreply.github.com>
2024-04-07 15:31:18 -0400

    Updated parameters

--- huy/model/train.py
+++ huy/model/train.py
@@ -1,7 +1,5 @@
 import re
 import numpy as np
-import random 
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
@@ -9,8 +7,8 @@ from sklearn.neural_network import MLPClassifier
 import warnings
 warnings.filterwarnings('ignore')
 
-file_name = "/Users/hwey/Desktop/repo_thaigia/CSC311H5S/clean_dataset.csv"
-random_state = 42
+file_name = "/Users/hwey/Desktop/repo_thaigia/CSC311H5S/ML Coding Challenge/clean_dataset.csv"
+RANDOM_STATE = 42
 
 def to_numeric(s):
     """Converts string `s` to a float.
@@ -61,75 +59,79 @@ def cat_in_s(s, cat):
     """
     return int(cat in s) if not pd.isna(s) else 0 
 
-if __name__ == "__main__":
+def parse_data(filename: str) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Given the name of a csv file, returns the data it contains as a vectorized array 
+    of points, and an array of corresponding labels.
+    """
+    data = pd.read_csv(filename)
 
-    df = pd.read_csv(file_name)
+    data["Q1"] = data["Q1"].apply(get_number)
+    data["Q2"] = data["Q2"].apply(get_number)
+    data["Q3"] = data["Q3"].apply(get_number)
+    data["Q4"] = data["Q4"].apply(get_number)
 
-    # Clean numerics
+    data["Q6"] = data["Q6"].apply(get_number_list_clean)
 
-    df["Q7"] = df["Q7"].apply(to_numeric).fillna(0)
-
-    # Clean for number categories
-
-    df["Q1"] = df["Q1"].apply(get_number)
-
-    # Create area rank categories
-
-    df["Q6"] = df["Q6"].apply(get_number_list_clean)
-
+    data["Q7"] = data["Q7"].apply(to_numeric).fillna(0)
+    data["Q8"] = data["Q8"].apply(to_numeric).fillna(0)
+    data["Q9"] = data["Q9"].apply(to_numeric).fillna(0)
+    
     temp_names = []
     for i in range(1,7):
         col_name = f"rank_{i}"
         temp_names.append(col_name)
-        df[col_name] = df["Q6"].apply(lambda l: find_area_at_rank(l, i))
-
-    del df["Q6"]
-
-    # Create category indicators
+        data[col_name] = data["Q6"].apply(lambda l: find_area_at_rank(l, i))
+    del data["Q6"]
 
     new_names = []
-    for col in ["Q1"] + temp_names:
-        indicators = pd.get_dummies(df[col], prefix=col)
+    for col in ["Q1", "Q2", "Q3", "Q4"] + temp_names:
+        values = [-1, 1, 2, 3, 4, 5, 6] if col in temp_names else [-1, 1, 2, 3, 4, 5]
+        indicators = pd.get_dummies(pd.Series(data[col], dtype=pd.CategoricalDtype(categories=values)), prefix=col)
         new_names.extend(indicators.columns)
-        df = pd.concat([df, indicators], axis=1)
-        del df[col]
-
-    # Create multi-category indicators
+        data = pd.concat([data, indicators], axis=1)
+        del data[col]
 
     for cat in ["Partner", "Friends", "Siblings", "Co-worker"]:
       cat_name = f"Q5{cat}"
       new_names.append(cat_name)
-      df[cat_name] = df["Q5"].apply(lambda s: cat_in_s(s, cat))
+      data[cat_name] = data["Q5"].apply(lambda s: cat_in_s(s, cat))
+    del data["Q5"]
+    
+    data = data[new_names + ["Q7", "Label"]]
 
-    del df["Q5"]
+    data = data.sample(frac=1, random_state=RANDOM_STATE)
 
-    # Prepare data for training - use a simple train/test split for now
+    x = data.drop("Label", axis=1).values
+    t = data["Label"]
 
-    df = df[new_names + ["Q7", "Label"]]
+    return x, t
 
-    df = df.sample(frac=1, random_state=random_state)
-
-    x = df.drop("Label", axis=1).values
-    y = pd.get_dummies(df["Label"].values)
-
+if __name__ == "__main__":
+    # parse data from csv file
+    x, y = parse_data(file_name)
+   
+    # split data into training and test sets
     n_train = 1200
-
     x_train = x[:n_train]
     y_train = y[:n_train]
-
     x_test = x[n_train:]
     y_test = y[n_train:]
 
+    # train Sklearn MLPClassifier with the given data
     model = MLPClassifier()
     model.fit(x_train, y_train)
+
+    # report validation accuracy
     print(model.score(x_test, y_test))
 
+    # extract sklearn model parameters
     W1 = model.coefs_[0]
     W2 = model.coefs_[1]
-
     b1 = model.intercepts_[0]
     b2 = model.intercepts_[1]
 
+    # report weights and parameters used in sklearn model 
     print(W1.tolist())
     print(W2.tolist())
     print(b1.tolist())
